d37bb1d41c9c7044f86ac64b53c4f8ee45ea0a06
yvonne-dong <dongy987@newschool.edu>
2018-02-01 00:13:26 -0500

    update

---
+++ week02/ownNode/main.js
@@ -0,0 +1,3 @@
+var test = require('./own');
+
+test.hi();
---
+++ week02/ownNode/own.js
@@ -0,0 +1,7 @@
+exports.hi = function(){
+	console.log("TEST");
+	for (var i = 0; i < 10; i ++){
+		console.log("*");
+	}
+	console.log("I GUESS IT WORKS : ))");
+}
