b3219a47ebf755ba2f7d8abe137cdac8dd2faf07
yvonne-dong <dongy987@newschool.edu>
2018-02-01 15:28:45 -0500

    date+time

--- week02/ownNode/main.js
+++ week02/ownNode/main.js
@@ -1,3 +1,15 @@
-var test = require('./own');
+var http = require('http');
+var date = require('./own');
 
-test.hi();
+const PORT = 8100;
+
+function handleRequest(request, response){
+	response.write("DATE + TIME: " + date.myDateTime() + ": ))");
+	response.end();
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function(){
+	console.log("Server listening on: http://localhost:%s", PORT)
+});
--- week02/ownNode/own.js
+++ week02/ownNode/own.js
@@ -1,7 +1,3 @@
-exports.hi = function(){
-	console.log("TEST");
-	for (var i = 0; i < 10; i ++){
-		console.log("*");
-	}
-	console.log("I GUESS IT WORKS : ))");
-}
+exports.myDateTime = function(){
+	return Date();
+};
